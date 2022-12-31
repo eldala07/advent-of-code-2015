f6f8c22af13e2229142f08d466222b29175abb43
Dimitri Uytterhoeven <dimitri.uytterhoeven@gmail.com>
2022-12-31 16:01:38 +0000

    improve indentation day02

--- day02/day02.py
+++ day02/day02.py
@@ -18,22 +18,22 @@ vectors = [[int(y) for y in x.split("x")] for x in s.split("\n")]
 def part_one(vectors):
     paper_surface = 0
     for [l, w, h] in vectors:
-      area1, area2, area3 = l*w, w*h, h*l
-      slack = min(area1, area2, area3)
-      paper = 2 * (area1 + area2 + area3)
-      paper_surface += paper + slack
+        area1, area2, area3 = l*w, w*h, h*l
+        slack = min(area1, area2, area3)
+        paper = 2 * (area1 + area2 + area3)
+        paper_surface += paper + slack
     return paper_surface
 
 def part_two(vectors):
     ribbon_length = 0
     for [l, w, h] in vectors:
-      sides = [l, w, h]
-      side_1 = min(sides)
-      sides.remove(side_1)
-      side_2 = min(sides)
-      present_ribbon = 2 * (side_1 + side_2)
-      bow_ribbon = l*w*h
-      ribbon_length += present_ribbon + bow_ribbon
+        sides = [l, w, h]
+        side_1 = min(sides)
+        sides.remove(side_1)
+        side_2 = min(sides)
+        present_ribbon = 2 * (side_1 + side_2)
+        bow_ribbon = l*w*h
+        ribbon_length += present_ribbon + bow_ribbon
     return ribbon_length
 
 print(part_one(vectors))
